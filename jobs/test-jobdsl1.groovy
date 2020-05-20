a5d2c9a714e48bc03caaecc883422944c9e7e061
Abel Godoy <somer.h2@gmail.com>
2020-05-20 17:40:43 -0400

    agregado prueba para jobdsl

---
+++ jobs/test-jobdsl1.groovy
@@ -0,0 +1,24 @@
+project_name = "Jenkins-jobdsl"
+repo = "https://github.com/s0m3r/test_jenkins.git"
+repo_name = "repo1"
+
+pipelineJob(project_name){
+	definition{
+		triggers{
+			scm('H/1 * * * *')
+		}
+
+		cpsScm{
+			scm{
+				git{
+					remote{
+						name(repo_name)
+						url(repo)
+					}
+				}
+				scriptPath("Jenkinsfile")
+
+			}
+		}
+	}
+}
